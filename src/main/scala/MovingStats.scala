773359400a951ac68dd84ab6eb9ea31257dab8d1
TingLiu6 <tliu6@luc.edu>
2018-03-02 00:08:59 -0600

    Finished functions and debugged code

--- src/main/scala/MovingStats.scala
+++ src/main/scala/MovingStats.scala
@@ -1,29 +1,42 @@
 
 
+import scala.collection.immutable.Queue
+
 /** Reads lines and prints cumulative length of all lines so far along with most recent line itself. */
-object CumulativeLengthFunctionalModular extends Main{
+object CumulativeLengthFunctionalModular extends Main {
 
   // def iterate[T](start: T)(f: (T) ⇒ T): Iterator[T]
-  // def scanLeft[B](z: B)(op: (B, A) ⇒ B): Iterator[B]
+  // def scanLeft[B](z: B)(op: B ⇒ B): Iterator[B]
   // https://github.com/scala/scala/blob/v2.12.4/src/library/scala/collection/Iterator.scala#L595
 
   /**
-    * Simple implementation of `scanLeft` using `iterate` and `takeWhile`.
-    * For `takeWhile` to work, this requires lifting the resulting iterator to `Option`.
-    * Alternatively, one could use `null`, but that would be less stylish.
-    */
-  def scanLeftUsingIterate(it: Iterator[Int])(z: (Int,Int))(op: (Int,Int)=>(Int,Int)): Iterator[(Int,Int)] =
+   * Simple implementation of `scanLeft` using `iterate` and `takeWhile`.
+   * For `takeWhile` to work, this requires lifting the resulting iterator to `Option`.
+   * Alternatively, one could use `null`, but that would be less stylish.
+   */
+  def scanLeftUsingIterate(it: Iterator[Int])(z: ((Queue[Int], Int, Int)))(op: ((Queue[Int], Int, Int)) => ((Queue[Int], Int, Int))):
+  Iterator[((Queue[Int], Int, Int))] =
+
     Iterator.iterate(Option(z)) {
       case Some(r) =>
-        if (it.hasNext) Option(op(r, it.next())) else None
+        if (it.hasNext)
+
+          Option(op((r._1, it.next(), r._3)))
+
+        else None
+
     } takeWhile (_.isDefined) map (_.get)
 
-  def accumulateCount(acc: Int, line: Int) = (acc, line)
+  def createQueue(stat: (Queue[Int], Int, Int)) = {
 
-  def run(lines: Iterator[Int]): Iterator[(Int, Int)] =
-    scanLeftUsingIterate(lines)("dummy", 0)(accumulateCount).drop(1)
+    (stat._1.enqueue(stat._2), stat._2, stat._3 + 1)
 
-  //scanLeftUsingIterate(lines)("dummy", 0)(accumulateCount).drop()
 
+
+  }
+
+  def run(numbers: Iterator[Int]): Iterator[((Queue[Int], Int, Int))] =
+
+    scanLeftUsingIterate(numbers)(Queue(0), 0, 0)(createQueue).drop(1)
 
 }
--- src/main/scala/common.scala
+++ src/main/scala/common.scala
@@ -1,20 +1,40 @@
-
+import scala.collection.immutable.Queue
 
 /** Defines a dependency (plug-in contract) on a run method that processes an input stream. */
 trait Task {
-  def run(input: Iterator[Int]): Iterator[(Int,Int)]
+  def run(input: Iterator[Int]): Iterator[((Queue[Int], Int, Int))]
+
 }
 
 /**
-  * Provides a reusable main task tied to stdin and stdout.
-  * Depends on a suitable run method.
-  */
+ * Provides a reusable main task tied to stdin and stdout.
+ * Depends on a suitable run method.
+ */
 trait Main extends Task {
   def main(args: Array[String]): Unit = {
+    import sun.misc.Signal
+    Signal.handle(new Signal("PIPE"), _ => scala.sys.exit())
+
     val lines = scala.io.Source.stdin.getLines
     val words = lines.flatMap(_.split("\\W+"))
     val numbers = words.map(_.toInt)
     val result = run(numbers)
-    result.foreach(println)
+
+    for (item <- result) {
+
+      print(item._2 + " " + item._3 + " ")
+
+      for (winsize <- args.map(_.toInt)) {
+        if (item._1.length - 1 >= winsize) {
+          val newqueue = item._1.drop(item._1.length - winsize)
+          print(newqueue.min * 1.0 + " " + newqueue.sum * 1.0 / newqueue.length + " " + newqueue.max * 1.0 + " ")
+        } else
+          print("? ? ? ")
+
+      }
+
+      println()
+
+    }
   }
 }
